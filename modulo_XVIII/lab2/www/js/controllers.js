77d850ae2f6508418f1d34da214979774bec85f1
alexospina7 <alexospina7@hotmail.com>
2016-08-06 12:32:24 -0500

    clase 8

---
+++ modulo_XVIII/lab2/www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('app.controllers', ['ngCordova'])
+  
+.controller('pageCtrl', ['$scope', '$stateParams','$cordovaNetwork', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams,$cordovaNetwork) {
+
+$scope.clickOnline=function(){
+$scope.type=$cordovaNetwork.getNetwork();
+	$scope.isonline=$cordovaNetwork.isOnline();
+	$scope.isoffline=$cordovaNetwork.isOffline();
+}
+
+}])
+ 
